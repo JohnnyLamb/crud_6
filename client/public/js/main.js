720a9e919e1e6678bd4e3a50dc57f2450dff32b9
johnny lamb <agnusjack@gmail.com>
2015-09-14 19:24:12 -0600

    got some routes and added some client side ajax

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,3 +3,19 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+$('form').on('submit',function(e){
+  e.preventDefault();
+  console.log("hey");
+  var payload = {
+    name:$('#name').val(),
+    age:$('#age').val(),
+
+  };
+  $.post('api/users', payload,function(data){
+    console.log(payload);
+
+  });
+
+
+});
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,9 +3,36 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = require('../models/users');
 
-
+// get ALL users
 router.get('/users', function(req, res, next) {
-  res.send('hello');
+  User.find(function(err,userData){
+      if(err){
+        res.json({
+          'message':err
+        });
+      }else{
+        res.json(userData);
+      }
+  });
 });
+
+router.post('/users',function(req,res,next){
+  newUser = new User ({
+    name:req.body.name,
+    age:req.body.age,
+    active:req.body.active,
+  });
+  newUser.save(function(err,userData){
+    if(err){
+      res.json({
+        'message':err
+      });
+    } else {
+      res.json(userData);
+    }
+  });
+
+});
+
 
 module.exports = router;
